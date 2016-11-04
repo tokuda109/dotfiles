5cf707ed7813986ee87a86098de31ffb3896fe48
tokuda109 <tokuda109@gmail.com>
2016-11-04 18:54:16 +0900

    pyenvの設定

--- zsh/python.zsh
+++ zsh/python.zsh
@@ -1,8 +1,9 @@
 
+export PATH=$PYENV_ROOT/bin:$PATH
+
+export PYTHONPATH=$(brew --prefix)/lib/python2.7/site-packages
+
+eval "$(pyenv init -)"
+
 # 実行時に.py[co]を生成しないようにする
 export PYTHONDONTWRITEBYTECODE=1
-
-if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
-  export WORKON_HOME=$HOME/.virtualenvs
-  source /usr/local/bin/virtualenvwrapper.sh
-fi
