9bf3b1a7ceb053ab473451075758e330688f25bc
tokuda109 <tokuda109@gmail.com>
2017-05-17 14:37:01 +0900

    Pyenv の設定

--- zsh/python.zsh
+++ zsh/python.zsh
@@ -1,11 +1,10 @@
 
+export PYENV_ROOT=$HOME/.pyenv
 export PATH=$PYENV_ROOT/bin:$PATH
 
-export PYTHONPATH=$(brew --prefix)/lib/python2.7/site-packages
+eval "$(pyenv init -)"
 
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
-
-eval "$(pyenv init -)"
 
 # 実行時に.py[co]を生成しないようにする
 export PYTHONDONTWRITEBYTECODE=1
