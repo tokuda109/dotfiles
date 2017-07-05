dac69f42f2acfe8044ea2281aef6385d03549e46
tokuda109 <tokuda109@gmail.com>
2017-07-05 12:11:27 +0900

    Fastlane の導入

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -3,3 +3,7 @@ export MANPATH=/usr/local/share/man:$MANPATH
 
 export PATH=/usr/local/opt/opencv3/bin:$PATH
 export PATH=node_modules/.bin:$PATH
+
+if [[ -d "${HOME}/.fastlane" ]]; then
+  export PATH=$HOME/.fastlane/bin:$PATH
+fi
