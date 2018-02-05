d7519cae77e371fa71ecfa2aecad1793ed9c33b8
tokuda109 <tokuda109@gmail.com>
2018-02-05 10:46:57 +0900

    node_modulesへのパス設定の記述する場所を変更

--- zsh/node.zsh
+++ zsh/node.zsh
@@ -1,4 +1,5 @@
 
 export NVM_DIR=$HOME/.nvm
+export PATH=node_modules/.bin:$PATH
 
 source $(brew --prefix nvm)/nvm.sh
--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,9 +1,6 @@
 
 export MANPATH=/usr/local/share/man:$MANPATH
 
-export PATH=/usr/local/opt/opencv3/bin:$PATH
-export PATH=node_modules/.bin:$PATH
-
 if [[ -d "${HOME}/.fastlane" ]]; then
   export PATH=$HOME/.fastlane/bin:$PATH
 fi
