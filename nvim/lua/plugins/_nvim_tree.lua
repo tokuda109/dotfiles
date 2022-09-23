bc41e7dddbf31674fd35fb9ab9dcf7234118a8f5
Tsuyoshi Tokuda <108762+tokuda109@users.noreply.github.com>
2022-09-23 09:01:54 +0900

    tweak nvim-tree settings

--- nvim/lua/plugins/_nvim_tree.lua
+++ nvim/lua/plugins/_nvim_tree.lua
@@ -3,6 +3,20 @@ if not status_ok then
   return
 end
 
-nvim_tree.setup({})
+-- disable netrw at the very start of your init.lua
+vim.g.loaded = 1
+vim.g.loaded_netrwPlugin = 1
+
+nvim_tree.setup({
+  actions = {
+    open_file = {
+      quit_on_open = true,
+      window_picker = {
+        enable = true,
+      }
+    }
+  },
+  sort_by = 'case_sensitive'
+})
 
 vim.api.nvim_set_keymap('n', 't', '<cmd>lua require\'nvim-tree\'.toggle(false, true)<cr>', { noremap = true })
