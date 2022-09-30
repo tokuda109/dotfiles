30e271e62dcdd6ab84efc5be6a50e3f0dc805b82
Tsuyoshi Tokuda <108762+tokuda109@users.noreply.github.com>
2022-10-01 08:57:42 +0900

    add ripgrep & live grep to recursively search

--- nvim/lua/plugins/_hop.lua
+++ nvim/lua/plugins/_hop.lua
@@ -5,4 +5,6 @@ end
 
 hop.setup({})
 
-vim.api.nvim_set_keymap('n', 'f', '<cmd>lua require\'hop\'.hint_words()<cr>', {})
+vim.api.nvim_set_keymap('n', 'FF', '<cmd>lua require\'hop\'.hint_words()<cr>', {})
+vim.api.nvim_set_keymap('n', 'Fc', '<cmd>lua require\'hop\'.hint_char1()<cr>', {})
+vim.api.nvim_set_keymap('n', 'Fl', '<cmd>lua require\'hop\'.hint_lines()<cr>', {})
--- nvim/lua/plugins/_telescope.lua
+++ nvim/lua/plugins/_telescope.lua
@@ -7,14 +7,25 @@ telescope.setup({
   defaults = {
     file_ignore_patterns = {
       'node_modules'
-    }
+    },
+    mappings = {
+      i = {},
+      n = {},
+    },
   },
   extensions = {
-    file_browser = {}
+    file_browser = {
+      hijack_netrw = true,
+      mappings = {
+        i = {},
+        n = {},
+      }
+    }
   }
 })
 
 telescope.load_extension('file_browser')
 
 vim.api.nvim_set_keymap('n', 'ff', '<cmd>lua require(\'telescope.builtin\').find_files()<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', 'fl', '<cmd>lua require(\'telescope.builtin\').live_grep()<cr>', { noremap = true })
 vim.api.nvim_set_keymap('n', 'fb', '<cmd>lua require(\'telescope\').extensions.file_browser.file_browser()<cr>', { noremap = true })
