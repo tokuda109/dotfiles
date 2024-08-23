a02a2de81b029ad9421548fb67d0307ad4f707f8
Tsuyoshi Tokuda <108762+tokuda109@users.noreply.github.com>
2024-08-23 14:22:25 +0900

    disable buggy plugins

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,11 +1,13 @@
 return {
-	{
-		"akinsho/bufferline.nvim",
-		keys = {
-			{ "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-			{ "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
-		},
-	},
+	-- {
+	-- 	"akinsho/bufferline.nvim",
+	-- 	keys = {
+	-- 		{ "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+	-- 		{ "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+	-- 	},
+	-- },
+	{ "akinsho/bufferline.nvim", enabled = false },
+	{ "folke/noice.nvim", enabled = false },
 	{
 		"nvimdev/dashboard-nvim",
 		event = "VimEnter",
