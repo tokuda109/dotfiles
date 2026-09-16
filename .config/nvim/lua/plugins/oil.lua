87f1e000960126f4ae1b3437fab1f4b4f7422081
Tsuyoshi Tokuda <108762+tokuda109@users.noreply.github.com>
2026-09-16 22:18:07 +0900

    feat: add .editorconfig and update oil.nvim configuration
    * configure oil.nvim to show hidden files and open project root

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,9 +1,21 @@
 return {
-	"stevearc/oil.nvim",
-	opts = {},
-	keys = {
-		{ "<leader>op", "<cmd>Oil<cr>", desc = "Open parent directory" },
-	},
-	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-	lazy = false,
+  "stevearc/oil.nvim",
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+  },
+  keys = {
+    { "<leader>op", "<cmd>Oil<cr>", desc = "Open parent directory" },
+    {
+      "<leader>oR",
+      function()
+        local root = vim.fn.getcwd()
+        require("oil").open(root)
+      end,
+      desc = "Open project root directory",
+    },
+  },
+  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+  lazy = false,
 }
