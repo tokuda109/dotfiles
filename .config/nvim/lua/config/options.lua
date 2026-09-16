354b323bfd55f94b0e7a9f45aa061faae010fcb3
Tsuyoshi Tokuda <108762+tokuda109@users.noreply.github.com>
2026-09-16 21:53:02 +0900

    feat: add oil.nvim plugin and update config

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.mapleader = " "
+
 -- Encoding
 vim.scriptencoding = "utf-8"
 vim.opt.encoding = "utf-8"
---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,9 @@
+return {
+	"stevearc/oil.nvim",
+	opts = {},
+	keys = {
+		{ "<leader>op", "<cmd>Oil<cr>", desc = "Open parent directory" },
+	},
+	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+	lazy = false,
+}
